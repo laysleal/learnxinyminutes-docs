01be65d03cddd79b78490592f8bed1b7eae390fd
laysleal <laysleal13@gmail.com>
2025-11-24 19:53:27 -0300

    [DAX/pt-br] add pt-br DAX guide
    Formatting test

--- pt-br/DAX.md
+++ pt-br/DAX.md
@@ -1,1 +1,18 @@
+---
+contributors:
+    - ["Lays Leal Correia", "https://github.com/laysleal"]
+---
+
+A linguagem DAX (Data Analysis Expressions) nasceu dentro do ecossistema
+Microsoft como parte do projeto de modelagem tabular lançado com o PowerPivot
+para Excel. Depois veio a se tornar a linguagem principal do Power BI.
+
+```DAX
+// Comentários em uma única linha começam com dupla barra, só é possível fazer
+// comentários a partir da segunda linha de código.
+
+/* Comentários de várias linhas podem ser escritas
+    usando /*
+    e finalizando com */
+*/
 
---
+++ pt-br/node_d0382f169ee7c.tmp.js
@@ -0,0 +1,20 @@
+---
+contributors:
+    - ["Lays Leal Correia", "https://github.com/laysleal"]
+---
+
+Python foi criada por Guido Van Rossum nos anos 1990. Ela é atualmente uma
+das linguagens mais populares existentes. Eu me apaixonei por
+Python por sua clareza sintática. É praticamente pseudocódigo executável.
+
+Observação: Este artigo trata de Python 3 especificamente. Verifique
+[aqui](../pythonlegacy/) se você pretende
+aprender o velho Python 2.7.
+
+```python
+# Comentários em uma única linha começam com uma cerquilha (também conhecido por sustenido).çp-9
+
+""" Strings de várias linhas podem ser escritas
+    usando três ", e são comumente usadas
+    como comentários.
+"""
